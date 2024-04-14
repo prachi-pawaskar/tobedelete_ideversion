7e7f68e3d47ef97beb994b841fb121e874673d5c
prachi.pawaskar <pawaskar.a.prachi@gmail,com>
2024-04-14 20:53:29 +0530

    Merge without PR

--- src/main/java/NewClass.java
+++ src/main/java/NewClass.java
@@ -2,5 +2,6 @@ public class NewClass {
     public static void main(String args[])  //static method
     {
         System.out.println("Test 2, direct change here also");
+        System.out.println("Test 2, this is for merge");
     }
 }
