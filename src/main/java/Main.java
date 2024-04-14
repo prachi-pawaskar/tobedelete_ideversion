c4fbfd76a14137c3aa3b29a80e84875f874b807c
Prachi Pawaskar <pawaskar.a.prachi@gmail.com>
2024-04-14 19:59:19 +0530

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String args[])  //static method
     {
-        System.out.println("Test change the print");
+        System.out.println("Test change the print, direct change in github");
     }
 }
